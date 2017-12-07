22e17432e119171ff99f04a9a7e728f3fd584963
Sohailbasha <shlbsh94@gmail.com>
2017-12-07 17:41:01 -0500

    Initial Commit

---
+++ ARPlayground/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MainViewController.swift
+//  ARPlayground
+//
+//  Created by Ilias Basha on 12/7/17.
+//  Copyright © 2017 Sohail. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class MainViewController: UIView {
+
+    @IBOutlet var sceneView: ARSCNView!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
